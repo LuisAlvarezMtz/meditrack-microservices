a6645adb79b4891564adc184043681a8a6ed92f8
LuisAlvarezMtz <luisangel.alvarezmtz@gmail.com>
2026-08-07 19:51:51 -0600

    feat(identity-service): add identity service with postgres db setup

---
+++ meditrack-identity-service/src/test/java/com/luisalvarez/meditrack/identityservice/MeditrackIdentityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisalvarez.meditrack.identityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeditrackIdentityServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
