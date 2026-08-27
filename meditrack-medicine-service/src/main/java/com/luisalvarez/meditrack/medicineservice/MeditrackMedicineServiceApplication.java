4e068644a85b55c542a0d4f334843ad68ba7d81d
LuisAlvarezMtz <luisangel.alvarezmtz@gmail.com>
2026-08-27 15:51:33 -0600

    feat(medicine-service): add medicine module with the initial setup

---
+++ meditrack-medicine-service/src/main/java/com/luisalvarez/meditrack/medicineservice/MeditrackMedicineServiceApplication.java
@@ -0,0 +1,13 @@
+package com.luisalvarez.meditrack.medicineservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeditrackMedicineServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeditrackMedicineServiceApplication.class, args);
+    }
+
+}
---
+++ meditrack-medicine-service/src/test/java/com/luisalvarez/meditrack/medicineservice/MeditrackMedicineServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisalvarez.meditrack.medicineservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeditrackMedicineServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
